cea550ed4bedee0a69aa2c7b67ead3c1258d387f
fernandacaramico <fernandacaramico@gmail.com>
2021-05-06 15:55:48 -0300

    altera arquivo instrucoes

--- API-projeto-site/routes/leituras.js
+++ API-projeto-site/routes/leituras.js
@@ -17,6 +17,7 @@ router.get('/ultimas/:idcaminhao', function(req, res, next) {
 	let instrucaoSql = "";
 
 	if (env == 'dev') {
+		// abaixo, escreva o select de dados para o Workbench
 		instrucaoSql = `select 
 		temperatura, 
 		umidade, 
@@ -26,6 +27,7 @@ router.get('/ultimas/:idcaminhao', function(req, res, next) {
 		where fkcaminhao = ${idcaminhao}
 		order by id desc limit ${limite_linhas}`;
 	} else if (env == 'production') {
+		// abaixo, escreva o select de dados para o SQL Server
 		instrucaoSql = `select top ${limite_linhas} 
 		temperatura, 
 		umidade, 
@@ -37,46 +39,48 @@ router.get('/ultimas/:idcaminhao', function(req, res, next) {
 	} else {
 		console.log("\n\n\n\nVERIFIQUE O VALOR DE LINHA 1 EM APP.JS!\n\n\n\n")
 	}
-
+	
 	sequelize.query(instrucaoSql, {
 		model: Leitura,
 		mapToModel: true 
-	  })
-	  .then(resultado => {
-			console.log(`Encontrados: ${resultado.length}`);
-			res.json(resultado);
-	  }).catch(erro => {
-			console.error(erro);
-			res.status(500).send(erro.message);
-	  });
+	})
+	.then(resultado => {
+		console.log(`Encontrados: ${resultado.length}`);
+		res.json(resultado);
+	}).catch(erro => {
+		console.error(erro);
+		res.status(500).send(erro.message);
+	});
 });
 
 
 router.get('/tempo-real/:idcaminhao', function(req, res, next) {
 	console.log('Recuperando caminhões');
-
+	
 	//var idcaminhao = req.body.idcaminhao; // depois de .body, use o nome (name) do campo em seu formulário de login
 	var idcaminhao = req.params.idcaminhao;
-
+	
 	let instrucaoSql = "";
-
+	
 	if (env == 'dev') {
+		// abaixo, escreva o select de dados para o Workbench
 		instrucaoSql = `select temperatura, umidade, FORMAT(momento,'HH:mm:ss') as momento_grafico, fkcaminhao from leitura where fkcaminhao = ${idcaminhao} order by id desc limit 1`;
 	} else if (env == 'production') {
+		// abaixo, escreva o select de dados para o SQL Server
 		instrucaoSql = `select top 1 temperatura, umidade, FORMAT(momento,'HH:mm:ss') as momento_grafico, fkcaminhao from leitura where fkcaminhao = ${idcaminhao} order by id desc`;
 	} else {
 		console.log("\n\n\n\nVERIFIQUE O VALOR DE LINHA 1 EM APP.JS!\n\n\n\n")
 	}
-
+	
 	console.log(instrucaoSql);
-
+	
 	sequelize.query(instrucaoSql, { type: sequelize.QueryTypes.SELECT })
-		.then(resultado => {
-			res.json(resultado[0]);
-		}).catch(erro => {
-			console.error(erro);
-			res.status(500).send(erro.message);
-		});
+	.then(resultado => {
+		res.json(resultado[0]);
+	}).catch(erro => {
+		console.error(erro);
+		res.status(500).send(erro.message);
+	});
 });
 
 // estatísticas (max, min, média, mediana, quartis etc)
