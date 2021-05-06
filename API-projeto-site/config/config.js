6295ba3151bad3b1ef17c2b05bc6cfab6ca3e544
fernandacaramico <fernandacaramico@gmail.com>
2021-05-06 18:09:56 -0300

    configs

--- API-projeto-site/config/config.js
+++ API-projeto-site/config/config.js
@@ -2,10 +2,10 @@ module.exports = {
   // Insira aqui seus dados do banco NA NUVEM AZURE
   production: {
     // altere APENAS username, password, database e host.
-    username: 'grupoteste',
-    password: '#Gfgrupot',
-    database: '202-grupot',
-    host: '202-grupot.database.windows.net',
+    username: 'SEU_USUARIO',
+    password: 'SUA_SENHA',
+    database: 'NOME DO SEU BD',
+    host: 'LINK PARA SEU BD, ALGO COMO minhadatabase.database.windows.net',
     dialect: 'mssql',
     xuse_env_variable: 'DATABASE_URL',
     dialectOptions: {
@@ -25,9 +25,9 @@ module.exports = {
   // Insira aqui seus dados do banco LOCAL - MySQL Workbench
   dev: {
     // altere APENAS username, password e database.
-    username: 'root',
-    password: 'bandtec',
-    database: 'enviaDados',
+    username: 'SEU USUARIO DO WORKBENCH, provavelmente é root',
+    password: 'SUA SENHA DO WORKBENCH',
+    database: 'SEU DATABASE DO CREATE DATABASE',
     host: '127.0.0.1',
     dialect: 'mysql',
     xuse_env_variable: 'DATABASE_URL',
